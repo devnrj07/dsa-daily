c47497e67ae1b0efdc9306c7acad5fe4f8e913d2
devnrj07 <niraj.mukta123@gmail.com>
2020-05-13 22:22:13 +0530

    day 37

---
+++ Each day code day!/leetcode/LCA of BT/Solution.java
@@ -0,0 +1,15 @@
+public class Solution{
+    public static void main(String[] args) {
+        
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p|| root ==q) return root;
+        //check on the left if p or q 🧯 
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left == null && right ==null) return null;
+        if(left !=null && right !=null) return root;
+        return left == null ? right: left;
+    }
+}
